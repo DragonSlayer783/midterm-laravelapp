41210a1af77af1edfcffa63c3946dce9c82b6eab
JacobD654 <JacobDiba654@gmail.com>
2022-03-27 18:23:52 -0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::get('/db-test', function () {
+    try {         
+         echo \DB::connection()->getDatabaseName();     
+    } catch (\Exception $e) {
+          echo 'None';
+    }
+});
+
 URL::forceScheme('https');
 
 Route::get('/', function () {
