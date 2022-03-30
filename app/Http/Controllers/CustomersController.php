14b5da6e7d968e7b8ac7ad5d632678ccb212ff61
DragonSlayer783 <JacobDiba654@gmail.com>
2022-03-30 15:09:06 -0700

    Update CustomersController.php

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -40,7 +40,7 @@ class CustomersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(FormBuilder $formBuilder)
     {
         
         $form = $formBuilder->create(CustomerForm::class);
