aacb30c8bde769c6d5af9d3e729321e0bac912c1
DragonSlayer783 <JacobDiba654@gmail.com>
2022-03-30 15:17:01 -0700

    Update list.blade.php

--- resources/views/customers/list.blade.php
+++ resources/views/customers/list.blade.php
@@ -9,7 +9,7 @@
 @section('content')
 <div class="card">
   <div class="card-body">
-    <table id="table" class="table table-bordered">
+    <table id="table" class="table table-bordered sortable">
       <thead>
         <tr>
           <th style="width: 10px">#</th><th>Name</th><th style="width: 40px">Action</th>
@@ -28,4 +28,8 @@
   </div>
 </div>
 <a href="{{ route('customers.create') }} " class="btn btn-primary" >Create</a>
+@stop
+
+@section('js')
+<script src="https://www.kryogenix.org/code/browser/sorttable/sorttable.js"></script>
 @stop
