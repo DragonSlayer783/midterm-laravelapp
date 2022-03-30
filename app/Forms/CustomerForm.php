37f5b00ddcc5e7ecea534e4c5ff546e8481f5be1
DragonSlayer783 <JacobDiba654@gmail.com>
2022-03-30 14:12:52 -0700

    added customer form

---
+++ app/Forms/CustomerForm.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Forms;
+
+use Kris\LaravelFormBuilder\Form;
+use Kris\LaravelFormBuilder\Field;
+
+
+class CustomerForm extends Form
+{
+    public function buildForm()
+    {
+        // Add fields here...
+    }
+}
