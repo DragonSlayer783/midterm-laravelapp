b65bf0bf223be95fd69df46dfd1e4e886400d9e5
DragonSlayer783 <JacobDiba654@gmail.com>
2022-03-29 19:05:47 -0700

    filled out some models

--- app/Models/Manuinfo.php
+++ app/Models/Manuinfo.php
@@ -5,7 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Contactinfo extends Model
+class Manuinfo extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name','dept', 'number','email'];
+
 }
