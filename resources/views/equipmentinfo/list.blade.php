fcf72bdb08b6b9b51f0bfccdbf16b9d30e70d705
DragonSlayer783 <JacobDiba654@gmail.com>
2022-03-30 17:19:06 -0700

    Update list.blade.php

--- resources/views/equipmentinfo/list.blade.php
+++ resources/views/equipmentinfo/list.blade.php
@@ -23,7 +23,7 @@
         @foreach($equipmentinfo AS $equipment)
         <tr>
           <td>{{ $equipment->id }}</td>
-          <td>{{ $equipment->manufacturer->name }}</td>
+          <td>{{ $equipment->manuinfo->name }}</td>
           <td>{{ ucwords($equipment->category) }}</td>
           <td>{{ $equipment->name }}</td>
           <td>{{ $equipment->num_of_notes() }}</td>
